54c212edd605e82a106bdbf75424bff6a7cbb38c
Иван А.А <I.Andryushchenko@i-teco.ru>
2023-04-02 21:43:07 +0300

    Create CloneHabrService & CloneHabr.Data

---
+++ CloneHabrService/CloneHabr.Data/ClonehabrDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloneHabr.Data
+{
+    public class ClonehabrDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<AccountSession> AccountSessions { get; set; }
+        public ClonehabrDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+
+}
